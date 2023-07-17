f520f1a0880cba041192e1bdf3cbe331db68ceb4
davidfarrow757 <david.farrow@colorado.edu>
2023-07-17 15:52:18 -0600

    Initial Commit

---
+++ Nevro/NevroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NevroApp.swift
+//  Nevro
+//
+//  Created by David Farrow on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct NevroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
