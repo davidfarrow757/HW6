5ebc3c3d5ef09ee08ae7ee67284b75afdeae773f
davidfarrow757 <david.farrow@colorado.edu>
2023-07-17 21:58:56 -0600

    views added

---
+++ Nevro/Services/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Nevro
+//
+//  Created by David Farrow, Andrew Carter.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
