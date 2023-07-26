c8b1580fc53f3fefa66910d33b8e82fbf0cdad59
davidfarrow757 <david.farrow@colorado.edu>
2023-07-26 10:47:03 -0600

    updates made

---
+++ Nevro/Core/Account/VehicleCardView.swift
@@ -0,0 +1,56 @@
+//
+//  VehicleCardView.swift
+//  Nevro
+//
+//  Created by David Farrow on 7/25/23.
+//
+
+import SwiftUI
+
+struct VehicleCardView: View {
+    // @EnvironmentObject var cartManager: CartManager
+    //var product: Product
+    var body: some View {
+        ZStack {
+            Color("Gray1")
+            ZStack(alignment: .bottomTrailing){
+                VStack(alignment: .leading){
+                    Image("fn1")
+                        .resizable()
+                        .frame(width: 175, height: 160)
+                        .cornerRadius(12)
+                    
+                    Text("Ford Mustang ")
+                        .font(.headline)
+                        .padding(.vertical,0.5)
+                    
+                    Text("2020")
+                        .foregroundColor(Color("AccentColor"))
+                        .font(.caption)
+                        .padding(.vertical, 0.5)
+                    
+                    Text("$200 per day")
+                        .bold()
+                }
+                Button{
+                    //cartManager.addToCart(product: product)
+                } label: {
+                    Image (systemName: "plus.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color("AccentColor"))
+                        .frame(width: 35, height: 35)
+                        .padding(.trailing)
+                }
+                
+            }
+        }
+        .frame(width: 185, height: 260)
+        .cornerRadius(15)
+    }
+}
+
+struct VehicleCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        VehicleCardView()
+    }
+}
--- Nevro/Core/Home/HomeView.swift
+++ Nevro/Core/Home/HomeView.swift
@@ -8,20 +8,50 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var isShowingDetailView = false
     var body: some View {
         ZStack {
-            //Profile Image
             Color("Background")
                 .edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack{
                 Image("Header")
+                    .frame(width: 190, height: 35)
+                    .padding(.top,35)
+                 Divider()
+        
                 ImageSliderView()
-                Spacer()
+                    .padding(.top,35)
+                
+                Button {
+                } label: {
+                    Text("Find your vehicle")
+                        .foregroundColor(.black)
+                        .frame(width: 300,height: 30)
+                        .background(.orange)
+                        .cornerRadius(100)
+                        .font(.title2)
                 }
+                
+                
+                
+                HStack {
+                    Text("Recently Added")
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Gray1"))
+                    Spacer()
+                    Image(systemName: "car")
+                        .foregroundColor(Color("Gray1"))
+                } .padding()
+                
+                
+                VehicleCardView()
+                
+
+                Spacer()
             }
         }
     }
+}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
