d2e6e17595b22d19a7bb6f8603ef998de3b6f16f
davidfarrow757 <david.farrow@colorado.edu>
2023-07-26 21:31:48 -0600

    Added comment in Edit Profile

--- Nevro/Core/More/EditAccountView.swift
+++ Nevro/Core/More/EditAccountView.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestoreSwift
 
 struct EditAccountView: View {
     @State private var currentPassword: String = ""
@@ -24,6 +28,32 @@ struct EditAccountView: View {
                     .foregroundColor(Color("Gray1"))
                     .font(.title)
                 
+            /* FireBase Guide
+            https://firebase.google.com/docs/auth/ios/manage-users#set_a_users_password
+            
+             let user = Auth.auth().currentUser
+             var credential: AuthCredential
+
+             // Prompt the user to re-provide their sign-in credentials
+             
+             
+             //Step 1. Re-authenticae
+             user?.reauthenticate(with: credential) { error in
+               if let error = error {
+                 // An error happened. Generate Alert
+               } else {
+                 // User re-authenticated.
+                // Step 2. Prompt the user to for new password and password confirmation
+                //Step 3. Check for match of new password and confimation
+                //Step 4. Click Submit and update password method
+                    Auth.auth().currentUser?.updatePassword(to: password) { error in
+                    // ...
+                    }
+               }
+             }
+           */
+                
+                //Current Password
                 SecureField("Enter a password", text: $currentPassword, prompt: Text("Current Password").foregroundColor(.orange))
                     .foregroundColor(Color("AccentColor"))
                     .padding(20)
@@ -33,6 +63,7 @@ struct EditAccountView: View {
                     .disableAutocorrection(true)
                     .overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.orange, lineWidth: 3).padding(.horizontal, 12).padding(.vertical, 5))
                 
+                //NewPassword
                 SecureField("Enter a password", text: $newPassword, prompt: Text("New Password").foregroundColor(.orange))
                     .foregroundColor(Color("AccentColor"))
                     .padding(20)
@@ -41,7 +72,8 @@ struct EditAccountView: View {
                     .multilineTextAlignment(.center)
                     .disableAutocorrection(true)
                     .overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.orange, lineWidth: 3).padding(.horizontal, 12).padding(.vertical, 5))
-                
+            
+                //Change Password
                 SecureField("Enter a password", text: $confirmPassword, prompt: Text("Confirm Password").foregroundColor(.orange))
                     .foregroundColor(Color("AccentColor"))
                     .padding(20)
@@ -50,7 +82,8 @@ struct EditAccountView: View {
                     .multilineTextAlignment(.center)
                     .disableAutocorrection(true)
                     .overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.orange, lineWidth: 3).padding(.horizontal, 12).padding(.vertical, 5))
-                
+           
+                //Submit Password Change
                 Button {
                     //Task{try await //registrationViewModel.createUser()}
                 } label: {
